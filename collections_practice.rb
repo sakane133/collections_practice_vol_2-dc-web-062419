15a89458027fa5782079ee4bf2fe45ba7093f8db
Samantha Kane <s.kane133@gmail.com>
2019-06-20 19:54:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
 # your code goes here
+def begins_with_r (array)
+  array.all? do |name|
+     name[0] == "r"
+  end
+end
+
+def contain_a (array)
+  array.select do |word|
+    word.include?("a")
+  end
+end 
+
+def first_wa(array)
+  array.find do |word|
+    word[0,2] == 'wa'
+  end
+end 
+
+def remove_non_strings(array)
+  array.delete_if do |word|
+    !word.is_a?(String)
+  end 
+end
+
+def count_elements(arr)
+
+  end
+
+def merge_data
+  
+end
+
+def find_cool
+  
+end
+
+def organize_schools
+  
+end
