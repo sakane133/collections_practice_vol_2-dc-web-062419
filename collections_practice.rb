586ec99d66ae392c0c754c55209bea32867f18d3
Samantha Kane <s.kane133@gmail.com>
2019-06-20 20:02:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,7 +24,9 @@ def remove_non_strings(array)
 end
 
 def count_elements(arr)
-
+  array.uniq.each {|i| count = 0
+        array.each {|i2| if i2 == i then count += 1 end}
+        i[:count] = count}
   end
 
 def merge_data
